050708d3d91a1085b29982262c8fcc5471cc5ad3
Guillaume Nervo <guillaume.nervo@gmail.com>
2021-12-08 11:11:29 +0100

    new contratsadded

--- src/constants/interfaces.ts
+++ src/constants/interfaces.ts
@@ -1,5 +1,6 @@
 import {
   ANGLE__factory,
+  AngleDistributor__factory,
   Erc20__factory,
   Erc20Bytes32__factory,
   FeeManager,
@@ -25,6 +26,7 @@ import {
 export const ERC20_Interface = Erc20__factory.createInterface();
 export const ERC20_BYTES32_Interface = Erc20Bytes32__factory.createInterface();
 
+export const AngleDistributor_Interface = AngleDistributor__factory.createInterface();
 export const Oracle_Interface = Oracle__factory.createInterface();
 export const Perpetual_Manager_Interface = PerpetualManagerFront__factory.createInterface();
 export const StableMasterFront_Interface = StableMasterFront__factory.createInterface();
