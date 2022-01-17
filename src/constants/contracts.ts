56e7c5629a24d5913c9912cef3b7dbeb87e2d609
David <teddav@users.noreply.github.com>
2022-01-17 10:51:01 +0100

    added data for new chains: FTM, AVAX and BSC (#28)

--- src/constants/contracts.ts
+++ src/constants/contracts.ts
@@ -1,8 +1,11 @@
 import { ChainId } from '../types';
-import LOCAL_ADDRESSES from './local.json';
-import MAINNET_CONTRACTS from './mainnet.json';
-import POLYGON_ADDRESSES from './polygon.json';
-import RINKEBY_TMP_ADDRESSES from './rinkebyTemporary.json';
+import AVALANCHE_ADDRESSES from './contracts_addresses/avalanche.json';
+import BSC_ADDRESSES from './contracts_addresses/bsc.json';
+import FANTOM_ADDRESSES from './contracts_addresses/fantom.json';
+import LOCAL_ADDRESSES from './contracts_addresses/local.json';
+import MAINNET_CONTRACTS from './contracts_addresses/mainnet.json';
+import POLYGON_ADDRESSES from './contracts_addresses/polygon.json';
+import RINKEBY_TMP_ADDRESSES from './contracts_addresses/rinkebyTemporary.json';
 
 const ContractsNames = [
   'FeeManager',
@@ -57,15 +60,18 @@ export type AngleContractsType = {
 } & {
   [key in typeof GlobalContracts[number]]?: string;
 } & {
-    ExternalStakings?: { tokenName: string; stakingContractAddress: string; poolContractAddress: string; liquidityGaugeAddress?: string }[];
-  } & {
-    Gauges?: { gaugeName: string; gaugeAddress: string; type: number }[];
-  };
+  ExternalStakings?: { tokenName: string; stakingContractAddress: string; poolContractAddress: string; liquidityGaugeAddress?: string }[];
+} & {
+  Gauges?: { gaugeName: string; gaugeAddress: string; type: number }[];
+};
 
 type TCONTRACTS_ADDRESSES = Readonly<{ [chainId in ChainId]: Readonly<AngleContractsType> }>;
 export const CONTRACTS_ADDRESSES: TCONTRACTS_ADDRESSES = {
   [ChainId.MAINNET]: MAINNET_CONTRACTS as AngleContractsType,
   [ChainId.POLYGON]: POLYGON_ADDRESSES as AngleContractsType,
+  [ChainId.AVALANCHE]: AVALANCHE_ADDRESSES as AngleContractsType,
+  [ChainId.FANTOM]: FANTOM_ADDRESSES as AngleContractsType,
+  [ChainId.BSC]: BSC_ADDRESSES as AngleContractsType,
   [ChainId.RINKEBY]: RINKEBY_TMP_ADDRESSES as AngleContractsType,
   [ChainId.LOCAL]: LOCAL_ADDRESSES as AngleContractsType,
 };
