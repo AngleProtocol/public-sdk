c9bba1cddfb6742b6fb80fdf112f23c33c04d75e
Pablo Veyrat <veyratpablo@gmail.com>
2022-06-27 12:30:56 +0200

    changing chains

--- src/constants/parameters/index.ts
+++ src/constants/parameters/index.ts
@@ -2,12 +2,16 @@ import { ChainId } from '../../types';
 import localhost from './localhost';
 import mainnet from './mainnet';
 import rinkeby from './rinkeby';
+import polygon from './polygon';
+import optimism from './optimism';
+import arbitrum from './arbitrum';
+import avalanche from './avalanche';
 import test from './test';
 import { GlobalParameters, PoolsParameters, StablesParameters } from './types';
 
 export default (
   chainID: number
-): { globalParameters: GlobalParameters; stablesParameters: StablesParameters; poolsParameters: PoolsParameters } => {
+): { globalParameters?: GlobalParameters; stablesParameters: StablesParameters; poolsParameters?: PoolsParameters } => {
   if (chainID === ChainId.RINKEBY) {
     return rinkeby;
   }
@@ -16,6 +20,18 @@ export default (
   }
   if (chainID === 1337) {
     return localhost;
+  }
+  if (chainID === ChainId.POLYGON) {
+    return polygon;
+  }
+  if (chainID === ChainId.OPTIMISM) {
+    return optimism;
+  }
+  if (chainID === ChainId.ARBITRUM) {
+    return arbitrum;
+  }
+  if (chainID === ChainId.AVALANCHE) {
+    return avalanche;
   }
   return test;
 };
