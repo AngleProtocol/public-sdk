d210815604e2b779803a8d1771a0e0a1615e04d9
Pablo Veyrat <50438397+sogipec@users.noreply.github.com>
2023-01-11 12:31:24 +0100

    refactor: add new collat names + rm tests (#157)

--- src/constants/contracts.ts
+++ src/constants/contracts.ts
@@ -30,8 +30,8 @@ export enum BorrowCollateral {
   'OP' = 'OP',
   'am3CRV' = 'am3CRV',
   'WAVAX' = 'WAVAX',
-  'cvxcrvFRAX' = 'cvxcrvFRAX',
-  'sdcrvFRAX' = 'sdcrvFRAX',
+  'cvx-crvFRAXUSDC' = 'cvx-crvFRAXUSDC',
+  'sd-crvFRAXUSDC' = 'sd-crvFRAXUSDC',
 }
 
 type BorrowCollateralType = {
@@ -81,9 +81,10 @@ type CollateralType = Readonly<
     Staking?: string;
     Strategies?: {
       [strategy in SimpleStrategy]?: string;
-    } & {
-      [strategy in LenderStrategy]?: LenderStrategyType;
-    };
+    } &
+      {
+        [strategy in LenderStrategy]?: LenderStrategyType;
+      };
   }>
 >;
 
@@ -114,59 +115,61 @@ type StablecoinType = Readonly<
   }>
 >;
 
-export type ContractsRegistryType = Readonly<{
-  [chainId in ChainId]: Partial<
-    {
-      ANGLE: string;
-      AngleDistributor: string;
-      AngleHelpers: string;
-      AngleRouter: string;
-      AngleRouterV2: string;
-      Core: string;
-      CoreBorrow: string;
-      FlashAngle: string;
-      FeeDistributor_sanUSDC_EUR: string;
-      GaugeController: string;
-      Governor: string;
-      Guardian: string;
-      KeeperMulticall: string;
-      KeeperRegistry: string;
-      MerkleRootDistributor: string;
-      Middleman: string;
-      MulticallWithFailure: string;
-      ProxyAdmin: string;
-      ProxyAdminGuardian: string;
-      RewardsDistributor: string;
-      SmartWalletWhitelist: string;
-      SurplusConverterSanTokens_EUR_USDC: string;
-      SurplusConverterUniV3_IntraCollaterals: string;
-      Timelock: string;
-      veANGLE: string;
-      veBoostProxy: string;
-      ExternalStakings: {
-        tokenName: string;
-        stakingContractAddress: string;
-        poolContractAddress: string;
-        liquidityGaugeAddress?: string;
-      }[];
-      Gauges: { gaugeName: string; gaugeAddress: string; type: number }[];
-      AMO: {
-        AMOMinter: string;
-        BPAMOs?: {
-          [key in AMO]?: AMOType;
+export type ContractsRegistryType = Readonly<
+  {
+    [chainId in ChainId]: Partial<
+      {
+        ANGLE: string;
+        AngleDistributor: string;
+        AngleHelpers: string;
+        AngleRouter: string;
+        AngleRouterV2: string;
+        Core: string;
+        CoreBorrow: string;
+        FlashAngle: string;
+        FeeDistributor_sanUSDC_EUR: string;
+        GaugeController: string;
+        Governor: string;
+        Guardian: string;
+        KeeperMulticall: string;
+        KeeperRegistry: string;
+        MerkleRootDistributor: string;
+        Middleman: string;
+        MulticallWithFailure: string;
+        ProxyAdmin: string;
+        ProxyAdminGuardian: string;
+        RewardsDistributor: string;
+        SmartWalletWhitelist: string;
+        SurplusConverterSanTokens_EUR_USDC: string;
+        SurplusConverterUniV3_IntraCollaterals: string;
+        Timelock: string;
+        veANGLE: string;
+        veBoostProxy: string;
+        ExternalStakings: {
+          tokenName: string;
+          stakingContractAddress: string;
+          poolContractAddress: string;
+          liquidityGaugeAddress?: string;
+        }[];
+        Gauges: { gaugeName: string; gaugeAddress: string; type: number }[];
+        AMO: {
+          AMOMinter: string;
+          BPAMOs?: {
+            [key in AMO]?: AMOType;
+          };
         };
-      };
-      bridges?: {
-        Anyswap?: string;
-        LayerZero?: string;
-        RainbowBridge?: string;
-        Synapse?: string;
-      };
-    } & {
-      [key in Stablecoin]?: StablecoinType;
-    }
-  >;
-}>;
+        bridges?: {
+          Anyswap?: string;
+          LayerZero?: string;
+          RainbowBridge?: string;
+          Synapse?: string;
+        };
+      } & {
+        [key in Stablecoin]?: StablecoinType;
+      }
+    >;
+  }
+>;
 
 export const CONTRACTS_ADDRESSES: ContractsRegistryType = {
   [ChainId.MAINNET]: MAINNET_CONTRACTS,
