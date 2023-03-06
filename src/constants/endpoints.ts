45e68abb205026e18f67679fcd3b7fb8c175b3e1
greedythib <thibaud.bruyelle@gmail.com>
2023-03-06 15:30:39 +0100

    feat: add prefix for tg endpoints

--- src/constants/endpoints.ts
+++ src/constants/endpoints.ts
@@ -1,8 +1,10 @@
 import { ChainId } from '../types/constants';
 import { SupportedChainsType } from '../types/merkl';
 
+const theGraphPrefix = 'https://api.thegraph.com/subgraphs/name/guillaumenervoxs/angle-merkl-reward';
+
 export const tgEndpoints: { [chainId in SupportedChainsType]: string } = {
-  [ChainId.MAINNET]: 'https://api.thegraph.com/subgraphs/name/guillaumenervoxs/angle-merkl-reward',
-  [ChainId.POLYGON]: 'https://api.thegraph.com/subgraphs/name/guillaumenervoxs/angle-merkl-reward-polygon',
-  [ChainId.OPTIMISM]: 'https://api.thegraph.com/subgraphs/name/guillaumenervoxs/angle-merkl-reward-optimism',
+  [ChainId.MAINNET]: theGraphPrefix,
+  [ChainId.POLYGON]: theGraphPrefix + '-polygon',
+  [ChainId.OPTIMISM]: theGraphPrefix + '-optimism',
 };
