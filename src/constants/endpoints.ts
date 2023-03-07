eee2f31f71f810b9148336b677c6961b38be065f
picodes <picodes@angle.money>
2023-03-07 14:31:29 +0100

    feat: Uniswap TheGraph endpoints

--- src/constants/endpoints.ts
+++ src/constants/endpoints.ts
@@ -8,3 +8,9 @@ export const tgEndpoints: { [chainId in SupportedChainsType]: string } = {
   [ChainId.POLYGON]: theGraphPrefix + '-polygon',
   [ChainId.OPTIMISM]: theGraphPrefix + '-optimism',
 };
+
+export const uniswapV3TheGraphEndpoints: { [chainId in SupportedChainsType]: string } = {
+  [ChainId.MAINNET]: 'https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v3',
+  [ChainId.POLYGON]: 'https://api.thegraph.com/subgraphs/name/ianlapham/uniswap-v3-polygon',
+  [ChainId.OPTIMISM]: 'https://api.thegraph.com/subgraphs/name/ianlapham/optimism-post-regenesis',
+};
