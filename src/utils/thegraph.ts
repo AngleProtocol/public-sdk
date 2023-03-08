b19e17f13344042ce7fbd09f8d7cfd5be15b6759
Thibaud <thibaud.bruyelle@gmail.com>
2023-03-08 15:31:50 +0100

    feat: add merkl wrapper fetcher utils (#188)
    * feat: add merkl wrapper fetcher utils
    
    * fix: rm potential duplicates from wrapper list

---
+++ src/utils/thegraph.ts
@@ -0,0 +1,32 @@
+import { gql, request } from 'graphql-request';
+
+import { tgEndpoints } from '../constants/endpoints';
+import { SupportedChainsType } from '../types/merkl';
+
+type PoolAddressesType = { arrakisPools: string[]; gammaPools: string[] };
+
+export const getMerklWrapperAddressesFromTheGraph = async (chainId: SupportedChainsType, pool: string): Promise<PoolAddressesType> => {
+  const tg_merkl = tgEndpoints[chainId];
+  const wrapperQuery = gql`
+    query Query($poolId: ID!) {
+      pool(id: $poolId) {
+        arrakisPools
+        gammaPools
+      }
+      s
+    }
+  `;
+  const poolData = await request<{
+    pool: PoolAddressesType;
+  }>(tg_merkl as string, wrapperQuery, {
+    poolId: pool,
+  });
+  const arrakisPools = poolData?.pool.arrakisPools.filter(function (elem, pos) {
+    return poolData?.pool.arrakisPools.indexOf(elem) == pos;
+  });
+  const gammaPools = poolData?.pool.gammaPools.filter(function (elem, pos) {
+    return poolData?.pool.gammaPools.indexOf(elem) == pos;
+  });
+
+  return { arrakisPools, gammaPools };
+};
