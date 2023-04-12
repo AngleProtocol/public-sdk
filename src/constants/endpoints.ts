b5740afdf3e7e88aa2863782c855984a687497b5
greedythib <thibaud.bruyelle@gmail.com>
2023-04-12 17:26:45 +0200

    feat: add arbitrum to merkl

--- src/constants/endpoints.ts
+++ src/constants/endpoints.ts
@@ -3,20 +3,23 @@ import { MerklSupportedChainIdsType } from '../types/merkl';
 
 const merklSubgraphPrefix = 'https://api.thegraph.com/subgraphs/name/guillaumenervoxs/uniswap-global-';
 export const merklSubgraphEndpoints: { [chainId in MerklSupportedChainIdsType]: string } = {
-  [ChainId.POLYGON]: merklSubgraphPrefix + 'polygon',
+  [ChainId.ARBITRUM]: merklSubgraphPrefix + 'arbitrum',
   [ChainId.OPTIMISM]: merklSubgraphPrefix + 'optimism',
+  [ChainId.POLYGON]: merklSubgraphPrefix + 'polygon',
 };
 
 export const uniswapV3TheGraphEndpoints: { [chainId in MerklSupportedChainIdsType]: string } = {
-  [ChainId.POLYGON]: 'https://api.thegraph.com/subgraphs/name/ianlapham/uniswap-v3-polygon',
+  [ChainId.ARBITRUM]: 'https://api.thegraph.com/subgraphs/name/ianlapham/arbitrum-minimal',
   [ChainId.OPTIMISM]: 'https://api.thegraph.com/subgraphs/name/ianlapham/optimism-post-regenesis',
+  [ChainId.POLYGON]: 'https://api.thegraph.com/subgraphs/name/ianlapham/uniswap-v3-polygon',
 };
 
 /** Fallback enddpoints
- * @dev cannot be used at the moment
+ * @dev it is not used at the moment
  */
 const merklFallbackSubgraphPrefix = 'https://api.thegraph.com/subgraphs/name/guillaumenervoxs/angle-merkl-reward';
 export const merklFallbackTGEndpoints: { [chainId in MerklSupportedChainIdsType]: string } = {
-  [ChainId.POLYGON]: merklFallbackSubgraphPrefix + '-polygon',
+  [ChainId.ARBITRUM]: merklFallbackSubgraphPrefix + '-arbitrum',
   [ChainId.OPTIMISM]: merklFallbackSubgraphPrefix + '-optimism',
+  [ChainId.POLYGON]: merklFallbackSubgraphPrefix + '-polygon',
 };
