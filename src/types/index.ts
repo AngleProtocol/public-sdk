8bb366d1897408f3b0bc451c7b2e21d1c5313082
Thibaud <thibaud.bruyelle@gmail.com>
2023-05-09 11:15:48 +0200

    fix: merkl utils correction (#202)

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,4 +2,4 @@ export * from './borrow';
 export { ActionType, AssetType, BASE_18, BigNumber_BASE, ChainId, NETWORK_LABELS } from './constants';
 export * from './merkl';
 export * from './token';
-export { BorrowActionType, RouterActionType, validateAndParseAddress } from './utils';
+export { BorrowActionType, findMerklAMMType, RouterActionType, validateAndParseAddress } from './utils';
