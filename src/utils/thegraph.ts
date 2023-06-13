a6c4cf38f6e084169e9e14653d9de3b29f382422
greedythib <thibaud.bruyelle@gmail.com>
2023-06-13 18:37:06 +0200

    rm comments

--- src/utils/thegraph.ts
+++ src/utils/thegraph.ts
@@ -27,34 +27,3 @@ export async function getVaultsForPoolId(poolAddress: string, graphUrl: string):
   }
   return vaultAddresses;
 }
-// export const getMerklWrapperAddressesFromTheGraph = async (
-//   chainId: MerklSupportedChainIdsType,
-//   amm: AMMType,
-//   pool: string
-// ): Promise<PoolAddressesType | null> => {
-//   const tg_merkl = merklSubgraphEndpoint[chainId][amm];
-//   if (amm === AMMType.UniswapV3) {
-//     const wrapperQuery = gql`
-//       query Query($poolId: ID!) {
-//         pool(id: $poolId) {
-//           arrakisPools
-//           gammaPools
-//         }
-//       }
-//     `;
-//     const poolData = await request<{
-//       pool: PoolAddressesType;
-//     }>(tg_merkl as string, wrapperQuery, {
-//       poolId: pool?.toLowerCase(),
-//     });
-//     const arrakisPools = poolData?.pool.arrakisPools.filter(function (elem, pos) {
-//       return poolData?.pool.arrakisPools.indexOf(elem) == pos;
-//     });
-//     const gammaPools = poolData?.pool.gammaPools.filter(function (elem, pos) {
-//       return poolData?.pool.gammaPools.indexOf(elem) == pos;
-//     });
-//     return { arrakisPools, gammaPools };
-//   } else {
-//     return null;
-//   }
-// };
